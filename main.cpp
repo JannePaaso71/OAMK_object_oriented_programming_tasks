342bb61117670a7fe25e89464eca86cd64acf777
JannePaaso71 <126378404+JannePaaso71@users.noreply.github.com>
2023-09-01 11:51:27 +0300

    kotitehtava 1b
    kotitethtava 1b

--- main.cpp
+++ main.cpp
@@ -3,17 +3,29 @@
 #include <ctime>
 using namespace std;
 
-int main() {
-    int guess = 0;
-    std::srand(time(0));
-    int randomNumber = std::rand() % 21;
+int game(int maxnum);
 
-    while(true) {
-        cout<<"Giveme a guess number: ";
+int main() {
+    int largestNumber= 0;
+    cout<<"What is the largest value of the guessed number?: ";
+    cin>>largestNumber;
+    int times = game(largestNumber);
+    cout<<"You tried "<<times<<" times"<<endl;
+    return 0;
+}
+
+int game(int maxnum) {
+    std::srand(time(0));
+    int randomNumber = std::rand() % maxnum;
+    randomNumber+=1;
+
+    for(int i=1; ; i++) {
+        int guess= 0;
+        cout<<"Give me a number: ";
         cin>>guess;
         if(guess==randomNumber) {
             cout<<"You gave me the correct answer!\n";
-            return false;
+            return i;
         }
         if(guess<randomNumber) {
             cout<<"Your answer is less than the correct number. Please, try again.\n";
@@ -22,5 +34,4 @@ int main() {
             cout<<"Your answer is more than the correct number. Please, try again.\n";
         }
     };
-    return 0;
 }
